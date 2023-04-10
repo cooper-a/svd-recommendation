2401cfaf753ef439a4b52a8f3201c8a7f9ff2f0c
Cooper Ang <cooper.km.a@gmail.com>
2023-04-09 20:28:51 -0400

    finalize code

--- plot_data.m
+++ plot_data.m
@@ -7,14 +7,14 @@ function plot_data(mat, data_type)
     %text(mat_3(:,1),mat_3(:,2),mat_3(:,3),num2str([0; 1; 2; 3; 4; 5]))
     % label each point with the data type]
     data_type_matrix = strcat(data_type, ' ', num2str((1:size(mat_3,1))'));
-    text(mat_3(:,1) + 0.01, mat_3(:,2) + 0.01, mat_3(:,3) + 0.01, data_type_matrix, 'FontSize', 12);
+    text(mat_3(:,1) + 0.01, mat_3(:,2) + 0.01, mat_3(:,3) + 0.01, data_type_matrix, 'FontSize', 16);
     set(gca,'XScale','linear','YScale','linear','ZScale','linear')
     zv = zeros(size(mat_3));
     hold on
     plot3([zv mat_3(:,1)]', [zv mat_3(:,2)]', [zv mat_3(:,3)]')
     scatter3(0, 0, 0, 100, 'g', 'p', 'filled')
-    text(0.01, 0.01, 0.01, "Origin")
+    text(0.01, 0.01, 0.01, "Origin", 'FontSize', 16)
     hold off
     view(43,30) % set camera view to azimuth=45, elevation=30 degrees
-    set(gca,'FontSize',12) % set font size to 16 points
+    set(gca,'FontSize',16) % set font size to 16 points
     
